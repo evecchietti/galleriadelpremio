de0202d38667351982ae5ac4a3b3d595a2c51bf5
Erika Vecchietti <erika.vecchietti@gmail.com>
2025-04-25 15:11:04 +0200

    test

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 document.addEventListener("DOMContentLoaded", function () {
   const toggle = document.querySelector(".menu-toggle");
-  const menu = document.querySelector("nav .menu");
+  const menu = document.querySelector(".menu");
   if (toggle && menu) {
     toggle.addEventListener("click", () => {
       menu.classList.toggle("show");
