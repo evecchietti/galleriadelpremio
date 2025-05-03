15b3e62c56e367e92f5320935a9b59bbf03e5c8a
Erika Vecchietti <erika.vecchietti@gmail.com>
2025-05-03 08:23:23 +0200

    test maggio 1

--- script.js
+++ script.js
@@ -1,29 +1,5 @@
 
-document.addEventListener("DOMContentLoaded", function () {
-  const toggle = document.querySelector(".menu-toggle");
-  const menu = document.querySelector(".menu");
-
-  toggle.addEventListener("click", function () {
-    menu.classList.toggle("show");
-  });
-
-  let currentSlide = 0;
-  const slides = document.querySelectorAll(".slide");
-  if (slides.length > 0) {
-    setInterval(() => {
-      slides[currentSlide].classList.remove("active");
-      currentSlide = (currentSlide + 1) % slides.length;
-      slides[currentSlide].classList.add("active");
-    }, 4000);
-  }
-});
-
-document.querySelectorAll(".has-dropdown > a").forEach(link => {
-  link.addEventListener("click", function (e) {
-    if (window.innerWidth <= 768) {
-      e.preventDefault();
-      const parent = this.parentElement;
-      parent.classList.toggle("active");
-    }
-  });
-});
+function toggleMenu() {
+  const menu = document.querySelector('.menu');
+  menu.classList.toggle('open');
+}
